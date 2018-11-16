59e8ca1856b63f6d1ecf97010fbab8577dc88b3f
Will Johannesen <willjohannesen@gmail.com>
2018-11-16 10:46:48 -0500

    Initial commit

---
+++ app/src/main/java/edu/temple/picturechooser/MainActivity.java
@@ -0,0 +1,208 @@
+package edu.temple.picturechooser;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView baseImageView, dataImageView;
+    Button baseButton, dataButton, submitButton;
+    Uri baseImageUri, dataImageUri;
+    TextView t;
+
+    String testMessage = "test test";
+
+    // which method am I using?
+    boolean multipartBody = true;
+    boolean file = false;
+
+    private static final int PICK_BASE_IMAGE = 100;
+    private static final int PICK_DATA_IMAGE = 101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        baseImageView = findViewById(R.id.imageView);
+        dataImageView = findViewById(R.id.imageView2);
+        baseButton = findViewById(R.id.button);
+        dataButton = findViewById(R.id.button2);
+        submitButton = findViewById(R.id.button3);
+        t = findViewById(R.id.textView);
+
+        baseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery(PICK_BASE_IMAGE);
+            }
+        });
+
+        dataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery(PICK_DATA_IMAGE);
+            }
+        });
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //sendInputsToApi();
+                //sendMessageToApi(testMessage);
+                getCapacity();
+            }
+        });
+    }
+
+    public void getCapacity() {
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl("https://stegosaurus.ml")
+                .addConverterFactory(GsonConverterFactory.create());
+        Retrofit retrofit = builder.build();
+        StegosaurusService client = retrofit.create(StegosaurusService.class);
+        if(baseImageUri != null) {
+            Call<String> call = client.howManyBytes(prepareFilePart("image", baseImageUri), false);
+            call.enqueue(new Callback<String>() {
+                @Override
+                public void onResponse(Call<String> call, Response<String> response) {
+                    t.setText(response.body());
+                }
+
+                @Override
+                public void onFailure(Call<String> call, Throwable t) {
+                    Toast.makeText(MainActivity.this, "Can't get capacity", Toast.LENGTH_SHORT).show();
+                    Log.i("Throwable", t.toString());
+                }
+            });
+        }
+    }
+
+    public void sendMessageToApi(String message) {
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl("https://stegosaurus.ml")
+                .addConverterFactory(GsonConverterFactory.create());
+        Retrofit retrofit = builder.build();
+        StegosaurusService client = retrofit.create(StegosaurusService.class);
+        Call<String> call = client.echoResponse(message);
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                t.setText(response.body());
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void sendInputsToApi(){
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl("https://stegosaurus.ml")
+                .addConverterFactory(GsonConverterFactory.create());
+
+        Retrofit retrofit = builder.build();
+
+        StegosaurusService client = retrofit.create(StegosaurusService.class);
+
+        if (baseImageUri != null && dataImageUri != null) {
+
+            if(multipartBody) {
+                Call<String> call = client.insertPhoto(
+                        prepareFilePart("base", baseImageUri),
+                        prepareFilePart("data", dataImageUri),
+                        "the key"
+                );
+                call.enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+                        t.setText(response.body());
+                    }
+
+                    @Override
+                    public void onFailure(Call<String> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+            else if (file) {
+                File baseFile = new File(baseImageUri.toString());
+                File dataFile = new File(dataImageUri.toString());
+                Call<String> call = client.insertPhotoFile(baseFile, dataFile, "the key");
+                call.enqueue(new Callback<String>() {
+                    @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+                        t.setText(response.body());
+                    }
+
+                    @Override
+                    public void onFailure(Call<String> call, Throwable t) {
+                        Toast.makeText(MainActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+
+        }
+        else
+            Toast.makeText(this, "Please enter all inputs", Toast.LENGTH_SHORT).show();
+
+    }
+
+    private MultipartBody.Part prepareFilePart(String partName, Uri fileUri) {
+        File file = FileUtils.getFile(this, fileUri);
+
+        // create RequestBody instance from file
+        RequestBody requestFile =
+                RequestBody.create(
+                        MediaType.parse(getContentResolver().getType(fileUri)),
+                        file
+                );
+
+        // MultipartBody.Part is used to send also the actual file name
+        return MultipartBody.Part.createFormData(partName, file.getName(), requestFile);
+    }
+
+    public void openGallery(int resultCode){
+        Intent gallery = new Intent (Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(gallery, resultCode);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK && requestCode == PICK_BASE_IMAGE) {
+            baseImageUri = data.getData();
+            baseImageView.setImageURI(baseImageUri);
+        }
+
+        if(resultCode == RESULT_OK && requestCode == PICK_DATA_IMAGE) {
+            dataImageUri = data.getData();
+            dataImageView.setImageURI(dataImageUri);
+        }
+    }
+}
